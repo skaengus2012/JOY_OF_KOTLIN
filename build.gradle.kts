4d787e9352f9c331512ce88141800ec01b4614ed
Doohyun Nam <skaengus2012@naver.com>
2020-05-31 14:32:15 +0900

    Implement #1 - gradle 설정 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,25 @@
-group = "org.example"
+
+group = "com.github.skaengus2012"
 version = "1.0-SNAPSHOT"
 
+plugins {
+    kotlin("jvm") version "1.3.61"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
