d569097c626f9483e7a7780916a4a7081728c8f4
Doohyun Nam <skaengus2012@naver.com>
2020-11-03 04:22:33 +0900

    Implement #11 - '트리를 사용한 데이터 처리' 예제 완료

---
+++ src/main/kotlin/com/nlab/joyofkotlin/chapter10/Tree.kt
@@ -0,0 +1,270 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nlab.joyofkotlin.chapter10
+
+import com.nlab.joyofkotlin.chapter5.List
+import com.nlab.joyofkotlin.chapter5.foldLeft
+import com.nlab.joyofkotlin.chapter6.Option
+import com.nlab.joyofkotlin.chapter8.foldLeft
+import kotlin.math.abs
+import kotlin.math.max
+
+/**
+ * @author Doohyun
+ */
+sealed class Tree<out T : Comparable<@UnsafeVariance T>> {
+
+    abstract fun isEmpty(): Boolean
+    abstract fun max(): Option<T>
+    abstract fun min(): Option<T>
+    abstract fun <U> foldLeft(identity: U, f: (U) -> (T) -> U, g: (U) -> (U) -> (U)): U
+    abstract fun <U> foldRight(identity: U, f: (T) -> (U) -> (U), g: (U) -> (U) -> (U)): U
+    abstract fun <U> foldInOrder(identity: U, f: (U) -> (T) -> (U) -> (U)): U
+    abstract fun <U> foldPreOrder(identity: U, f: (T) -> (U) -> (U) -> (U)): U
+    abstract fun <U> foldPostOrder(identity: U, f: (U) -> (U) -> (T) -> U): U
+    abstract fun <U : Comparable<U>> map(f: (T) -> U): Tree<U>
+    abstract val size: Int
+    abstract val height: Int
+
+    protected abstract fun rotateRight(): Tree<T>
+    protected abstract fun rotateLeft(): Tree<T>
+
+    operator fun plus(element: @UnsafeVariance T): Tree<T> = when(this) {
+        is Empty -> TreeEx(left = Empty, value = element, right = Empty)
+        is TreeEx -> when {
+            element < value -> TreeEx(left = left + element, value = value, right = right)
+            element > value -> TreeEx(left = left, value = value, right = right + element)
+            else -> TreeEx(left = Empty, value = element, right = Empty)
+        }
+    }
+
+    operator fun contains(element: @UnsafeVariance T): Boolean = when(this) {
+        is Empty -> false
+        is TreeEx -> when {
+            value == element -> true
+            value > element -> left.contains(element)
+            else -> right.contains(element)
+        }
+    }
+
+    fun remove(element: @UnsafeVariance T): Tree<T> = when(this) {
+        Empty -> this
+        is TreeEx -> when {
+            value == element -> removeMerge(right, left)
+            value > element -> TreeEx(left.remove(element), value, right)
+            else -> TreeEx(left, value, right.remove(element))
+        }
+    }
+
+    fun merge(tree: Tree<@UnsafeVariance T>): Tree<T> = when(this) {
+        Empty -> tree
+        is TreeEx -> when(tree) {
+            Empty -> this
+            is TreeEx -> when {
+                tree.value < value -> {
+                    TreeEx(left.merge(TreeEx(tree.left, tree.value, Empty)), value, right).merge(tree.right)
+                }
+                tree.value > value -> {
+                    TreeEx(left, value, right.merge(TreeEx(Empty, tree.value, tree.right))).merge(tree.left)
+                }
+                else -> {
+                    TreeEx(left.merge(tree.left), value, right.merge(tree.right))
+                }
+            }
+        }
+    }
+
+    fun toListByDesc(): List<T> {
+        tailrec fun toListByDescRec(acc: List<T>, tree: Tree<T>): List<T> = when(tree) {
+            Empty -> acc
+            is TreeEx -> when(tree.left) {
+                Empty -> toListByDescRec(acc.construct(tree.value), tree.right)
+                is TreeEx -> toListByDescRec(acc, tree.rotateRight())
+            }
+        }
+
+        return toListByDescRec(List(), this)
+    }
+
+    fun toListByAsc(): List<T> {
+        tailrec fun toListByAscRec(acc: List<T>, tree: Tree<T>): List<T> = when(tree) {
+            Empty -> acc
+            is TreeEx -> when(tree.right) {
+                Empty -> toListByAscRec(acc.construct(tree.value), tree.left)
+                is TreeEx -> toListByAscRec(acc, tree.rotateLeft())
+            }
+        }
+
+        return toListByAscRec(List(), this)
+    }
+
+    private object Empty : Tree<Nothing>() {
+        override val size: Int = 0
+        override val height: Int = -1
+        override fun isEmpty(): Boolean = true
+        override fun max(): Option<Nothing> = Option.None
+        override fun min(): Option<Nothing> = Option.None
+        override fun <U> foldLeft(identity: U, f: (U) -> (Nothing) -> U, g: (U) -> (U) -> U): U = identity
+        override fun <U> foldRight(identity: U, f: (Nothing) -> (U) -> U, g: (U) -> (U) -> U): U = identity
+        override fun <U> foldInOrder(identity: U, f: (U) -> (Nothing) -> (U) -> U): U = identity
+        override fun <U> foldPreOrder(identity: U, f: (Nothing) -> (U) -> (U) -> U): U = identity
+        override fun <U> foldPostOrder(identity: U, f: (U) -> (U) -> (Nothing) -> U): U = identity
+        override fun <U : Comparable<U>> map(f: (Nothing) -> U): Tree<U> = this
+        override fun rotateRight(): Tree<Nothing> = this
+        override fun rotateLeft(): Tree<Nothing> = this
+        override fun toString(): String = "E"
+    }
+
+    private class TreeEx<out T : Comparable<@UnsafeVariance T>>(
+        val left: Tree<T>,
+        val value: T,
+        val right: Tree<T>
+    ) : Tree<T>() {
+        override val size: Int = 1 + left.size + right.size
+        override val height: Int = 1 + max(left.height, right.height)
+        override fun isEmpty(): Boolean = false
+        override fun max(): Option<T> = right.max().orElse { Option(value) }
+        override fun min(): Option<T> = left.min().orElse { Option(value) }
+        override fun <U> foldLeft(
+            identity: U,
+            f: (U) -> (T) -> U,
+            g: (U) -> (U) -> U
+        ): U = g(right.foldLeft(identity, f, g))(f(left.foldLeft(identity, f, g))(value))
+        override fun <U> foldRight(
+            identity: U,
+            f: (T) -> (U) -> U,
+            g: (U) -> (U) -> U
+        ): U = g(f(value)(left.foldRight(identity, f, g)))(right.foldRight(identity, f, g))
+        override fun <U> foldInOrder(
+            identity: U,
+            f: (U) -> (T) -> (U) -> U
+        ): U = f(left.foldInOrder(identity, f))(value)(right.foldInOrder(identity, f))
+        override fun <U> foldPreOrder(
+            identity: U,
+            f: (T) -> (U) -> (U) -> U
+        ): U = f(value)(left.foldPreOrder(identity, f))(right.foldPreOrder(identity, f))
+        override fun <U> foldPostOrder(
+            identity: U,
+            f: (U) -> (U) -> (T) -> U
+        ): U = f(left.foldPostOrder(identity, f))(right.foldPostOrder(identity, f))(value)
+        override fun <U : Comparable<U>> map(f: (T) -> U): Tree<U> = foldPreOrder(invoke()) { value: T ->
+            { result1: Tree<U> -> { result2: Tree<U> -> invoke(result1, f(value), result2) } }
+        }
+        override fun rotateRight(): Tree<T> = when(left) {
+            Empty -> this
+            is TreeEx -> TreeEx(left.left, left.value, TreeEx(left.right, value, right))
+        }
+        override fun rotateLeft(): Tree<T> = when(right) {
+            Empty -> this
+            is TreeEx -> TreeEx(TreeEx(left, value, right.left), right.value, right.right)
+        }
+        override fun toString(): String = "[$left $value $right]"
+    }
+
+    companion object {
+
+        operator fun <T : Comparable<T>> invoke(): Tree<T> = Empty
+
+        operator fun <T : Comparable<T>> invoke(list: List<T>): Tree<T> = foldLeft(list, invoke()) { acc: Tree<T> ->
+            { element: T -> acc + element }
+        }
+
+        operator fun <T : Comparable<T>> invoke(vararg elements: T): Tree<T> = invoke(List(*elements))
+
+        operator fun <T : Comparable<T>> invoke(left: Tree<T>, value: T, right: Tree<T>): Tree<T> = when {
+            isOrdered(left, value, right) -> TreeEx(left, value, right)
+            isOrdered(right, value, left) -> TreeEx(right, value, left)
+            else -> Tree(value).merge(left).merge(right)
+        }
+
+        private fun <T : Comparable<T>> isOrdered(
+            left: Tree<T>,
+            value: T,
+            right: Tree<T>
+        ): Boolean = left.max()
+            .flatMap { lMax -> right.min().map { rMin -> lt(lMax, value, rMin) } }
+            .getOrElse {
+                (left.isEmpty() && right.max().map { rMax -> lt(value, rMax) }.getOrElse { true })
+                        || (right.isEmpty() && left.max().map { lMax -> lt(lMax, value) }.getOrElse { true })
+            }
+
+        private fun <T : Comparable<T>> lt(a: T, b: T): Boolean = a < b
+        private fun <T : Comparable<T>> lt(a: T, b: T, c: T): Boolean = lt(a, b) && lt(b, c)
+
+        private fun <T : Comparable<T>> removeMerge(a: Tree<T>, b: Tree<T>): Tree<T> = when(a) {
+            Empty -> b
+            is TreeEx -> {
+                when(b) {
+                    Empty -> a
+                    is TreeEx -> {
+                        when {
+                            b.value < a.value -> TreeEx(removeMerge(a.left, b), a.value, a.right)
+                            else -> TreeEx(a.left, a.value, removeMerge(a.right, b))
+                        }
+                    }
+                }
+            }
+        }
+
+        private fun <T : Comparable<T>> isUnBalance(tree: Tree<T>): Boolean = when(tree) {
+            Empty -> false
+            is TreeEx -> abs(tree.left.height - tree.right.height) > (tree.size - 1) % 2
+        }
+
+        private fun log2nlz(n: Int) = when(n) {
+            0 -> 0
+            else -> 31 - Integer.numberOfLeadingZeros(n)
+        }
+
+        /**
+         * 데이-스타우트-워런 알고리즘
+         *
+         * 1. Tree 를 완전불균형 트리로 만든다.
+         * 2. 양쪽 균형이 맞도록 트리 회전 (트리 size 가 짝수라면 1차이, 홀수라면 동일)
+         * 3. 왼쪽, 오른쪽 트리 역시 동일한 처리 필요.
+         */
+        fun <T : Comparable<T>> balance(
+            tree: Tree<T>
+        ): Tree<T> = balanceHelper(foldLeft(tree.toListByDesc(), invoke()) { acc ->
+            { value -> acc + value }
+        })
+
+        private fun <T : Comparable<T>> balanceHelper(tree: Tree<T>): Tree<T> = when(tree) {
+            Empty -> tree
+            is TreeEx -> when {
+                tree.height <= log2nlz(tree.size) -> tree
+                else -> when {
+                    isUnBalance(tree) -> balanceHelper(balanceFirstLevel(tree))
+                    else -> TreeEx(balanceHelper(tree.left), tree.value, balanceHelper(tree.right))
+                }
+            }
+        }
+
+        private fun <T : Comparable<T>> balanceFirstLevel(tree: Tree<T>): Tree<T> = when(tree) {
+            Empty -> tree
+            is TreeEx -> when {
+                !isUnBalance(tree) -> tree
+                else -> when {
+                    tree.left.height < tree.right.height -> tree.rotateLeft()
+                    else -> tree.rotateRight()
+                }
+            }
+        }
+
+    }
+
+}
---
+++ src/test/kotlin/com/nlab/joyofkotlin/chapter10/TreeTest.kt
@@ -0,0 +1,217 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nlab.joyofkotlin.chapter10
+
+import com.nlab.joyofkotlin.chapter5.List
+import com.nlab.joyofkotlin.chapter5.concatViaFoldLeft
+import com.nlab.joyofkotlin.chapter5.concatViaFoldRight
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import kotlin.math.floor
+import kotlin.math.log
+import kotlin.math.log2
+
+/**
+ * @author Doohyun
+ */
+class TreeTest {
+
+    @Test
+    fun testContains() {
+        assertTrue(4 in Tree(2, 4, 10, 5))
+        assertFalse(11 in Tree(2, 4, 10, 5))
+    }
+
+    @Test
+    fun testSize() {
+        assertEquals(5, Tree(2, 3, 10, 5, 6).size)
+        assertEquals(5, Tree(5, 3, 10, 2, 6).size)
+        assertEquals(0, Tree<Int>().size)
+    }
+
+    @Test
+    fun testHeight() {
+        assertEquals(2, Tree(3, 2, 4, 1).height)
+        assertEquals(5, Tree(5, 6, 7, 8, 9, 10).height)
+        assertEquals(0, Tree(11).height)
+        assertEquals(-1, Tree<Int>().height)
+    }
+
+    @Test
+    fun testMax() {
+        assertEquals(4, Tree(3, 2, 4, 1).max().getOrElse { -1 })
+        assertEquals(10, Tree(5, 6, 7, 8, 9, 10).max().getOrElse { -1 })
+        assertEquals(11, Tree(11).max().getOrElse { -1 })
+        assertEquals(-1, Tree<Int>().max().getOrElse { -1 })
+    }
+
+    @Test
+    fun testMin() {
+        assertEquals(1, Tree(3, 2, 4, 1).min().getOrElse { -1 })
+        assertEquals(5, Tree(5, 6, 7, 8, 9, 10).min().getOrElse { -1 })
+        assertEquals(11, Tree(11).min().getOrElse { -1 })
+        assertEquals(-1, Tree<Int>().min().getOrElse { -1 })
+    }
+
+    @Test
+    fun testRemove() = with(Tree(7, 2, 20, 1, 5, 15, 24, 30, 13, 16, 21)) {
+        assertEquals(Tree(20, 15, 24, 13, 2, 1, 5, 16, 21, 30).toString(), remove(7).toString())
+        assertEquals(Tree(7, 2, 24, 1, 5, 21, 30, 15, 13, 16).toString(), remove(20).toString())
+        assertEquals(Tree(7, 2, 20, 5, 15, 24, 30, 13, 16, 21).toString(), remove(1).toString())
+        assertEquals(Tree(7, 2, 20, 1, 5, 15, 24, 13, 16, 21).toString(), remove(30).toString())
+    }
+
+    @Test
+    fun testMerge() {
+        assertEquals(
+            Tree(5, 1, 4, 3, 9, 7).toString(),
+            Tree(5, 1, 9).merge(Tree(4, 3, 7)).toString()
+        )
+
+        assertEquals(
+            Tree(5, 1, 4, 2, 3, 10, 6, 7, 8).toString(),
+            Tree(5, 1, 4, 10, 6).merge(Tree(7, 2, 3, 8)).toString()
+        )
+    }
+
+    @Test
+    fun testFold() = with(Tree(4, 2, 6, 1, 3, 5, 7)) {
+        assertEquals(
+            28,
+            foldLeft(
+                identity = 0,
+                f = { acc -> { num -> acc + num } },
+                g = { result1 -> { result2 -> result1 + result2 } }
+            )
+        )
+
+        assertEquals(
+            28,
+            foldRight(
+                identity = 0,
+                f = { num -> { acc -> acc + num } },
+                g = { result1 -> { result2 -> result1 + result2 } }
+            )
+        )
+
+        assertEquals(
+            List(7, 6, 5, 4, 3, 2, 1).toString(),
+            foldLeft(
+                identity = List<Int>(),
+                f = { acc -> { num -> acc.construct(num) } },
+                g = { result1 -> { result2 -> concatViaFoldLeft(result1, result2) } }
+            ).toString()
+        )
+
+        assertEquals(
+            List(4, 2, 1, 3, 6, 5, 7).toString(),
+            foldRight(
+                identity = List<Int>(),
+                f = { num: Int -> { acc -> acc.construct(num) } },
+                g = { result1 -> { result2 -> concatViaFoldRight(result1, result2) } }
+            ).toString()
+        )
+    }
+
+    @Test fun testFoldByOrder() = with(Tree(4, 2, 6, 1, 3, 5, 7)) {
+        assertEquals(
+            List(1, 2, 3, 4, 5, 6, 7).toString(),
+            foldInOrder(List<Int>()) { result1 ->
+                { num -> { result2 -> concatViaFoldRight(result1, result2.construct(num)) } }
+            }.toString()
+        )
+
+        assertEquals(
+            List(4, 2, 1, 3, 6, 5, 7).toString(),
+            foldPreOrder(List<Int>()) { num ->
+                { result1 -> { result2 -> concatViaFoldRight(result1, result2).construct(num) } }
+            }.toString()
+        )
+
+        assertEquals(
+            List(1, 3, 2, 5, 7, 6, 4).toString(),
+            foldPostOrder(List<Int>()) { result1 ->
+                { result2 -> { num -> concatViaFoldRight(result1, result2.reverse().construct(num).reverse()) } }
+            }.toString()
+        )
+    }
+
+    @Test fun testIdentity() = with(Tree(4, 2, 6, 1, 3, 5, 7)) {
+        assertEquals(
+            toString(),
+            foldInOrder(Tree<Int>()) { result1 -> { num -> { result2 -> Tree(result1, num, result2) } } }.toString()
+        )
+
+        assertEquals(
+            toString(),
+            foldPreOrder(Tree<Int>()) { num -> { result1 -> { result2 -> Tree(result1, num, result2) } } }.toString()
+        )
+
+        assertEquals(
+            toString(),
+            foldPostOrder(Tree<Int>()) { result1 -> { result2 -> { num -> Tree(result1, num, result2) } } }.toString()
+        )
+    }
+
+    @Test fun testMap() = with(Tree(4, 2, 6, 1, 3, 5, 7)) {
+        assertEquals(
+            Tree(6, 4, 8, 3, 5, 7, 9).toString(),
+            map { it + 2 }.toString()
+        )
+
+        assertEquals(
+            Tree(-4, -2, -6, -1, -3, -5, -7).toString(),
+            map { it * -1 }.toString()
+        )
+
+        assertEquals(
+            Tree(0).toString(),
+            map { it * 0 }.toString()
+        )
+    }
+
+    @Test fun testToList() = with(Tree(4, 2, 6, 1, 3, 5, 7)) {
+        assertEquals(List(7, 6, 5, 4, 3, 2, 1).toString(), toListByDesc().toString())
+        assertEquals(List(1, 2, 3, 4, 5, 6, 7).toString(), toListByAsc().toString())
+    }
+
+    @Test fun testBalance() {
+
+        with(Tree(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) {
+            assertEquals(
+                floor(log2(size.toFloat())).toInt(),
+                Tree.balance(this).height
+            )
+        }
+
+        with(Tree(1, 2, 3, 4, 5, 6, 7, 21, 9, 10, 11, 12, 13, 14, 16, 20)) {
+            assertEquals(
+                floor(log2(size.toFloat())).toInt(),
+                Tree.balance(this).height
+            )
+        }
+
+        with(Tree<Int>()) {
+            assertEquals(
+                -1,
+                Tree.balance(this).height
+            )
+        }
+
+    }
+
+}
